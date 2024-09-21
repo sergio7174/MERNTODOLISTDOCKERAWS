7509b6ca573a25da26c1393140c791c40cb64bdf
sergio7174 <sergiogmoncada@yahoo.com>
2024-09-21 15:56:01 -0400

    My first commit

---
+++ server.js
@@ -0,0 +1,50 @@
+const path = require('path');
+const dotenv = require('dotenv');
+const express = require('express');
+const bodyParser = require('body-parser');
+require('dotenv').config(); /***CALL DOTENV LIB */
+
+/*if(process.env.ENVIRONMENT !== 'production') {
+    require('dotenv').config()
+}*/
+
+const taskController = require('./controller/task.controller')
+
+const app = express();
+const port = process.env.PORT || 3080;
+
+app.use(express.static(path.join(__dirname, './ui/build')));
+app.use(express.static(path.join(__dirname)));
+app.use(bodyParser.json());
+
+app.get('/api/tasks', (req, res) => {
+    taskController.getTasks().then(data => res.json(data));
+});
+
+app.post('/api/task', (req, res) => {
+    console.log(req.body);
+    taskController.createTask(req.body.task).then(data => res.json(data));
+});
+
+app.put('/api/task', (req, res) => {
+    taskController.updateTask(req.body.task).then(data => res.json(data));
+});
+
+app.delete('/api/task/:id', (req, res) => {
+    taskController.deleteTask(req.params.id).then(data => res.json(data));
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './ui/build/index.html'));
+});
+
+
+
+app.listen(port, () => {
+    console.log(`Server listening on the port  ${port}`);
+})
+
+console.log('environment:    ', process.env.ENVIRONMENT);
+console.log('PORT:    ', process.env.PORT);
+console.log('MONGO_CONNECTION_STRING:    ', process.env.MONGO_CONNECTION_STRING);
+console.log('nombre:    ', process.env.nombre);
